e893540a97bd93fa19b02716cf81bab8dbbe76e9
Esa Pratama Putri <ptrprtm16@gmail.com>
2025-03-08 14:21:45 +0700

    praktikum 2.3 j4

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -10,24 +10,31 @@
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
             <th>ID</th>
-            <th>Username</th>
+            {{-- <th>Username</th>
             <th>Nama</th>
-            <th>ID Level Pengguna</th>
+            <th>ID Level Pengguna</th> --}}
         </tr>
         {{-- @foreach ($data as $d) --}}
-        @if ($data)
-            <tr>
-                <td>{{ $data->user_id }}</td>
-                <td>{{ $data->username }}</td>
-                <td>{{ $data->nama }}</td>
-                <td>{{ $data->level_id }}</td>
-            </tr>
-            {{-- @endforeach --}}
-        @else
-            <tr>
-                <td colspan="4">Data tidak ditemukan</td>
-            </tr>
-        @endif
+        {{-- @if ($data)
+        <tr>
+            <td>{{ $data->user_id }}</td>
+            <td>{{ $data->username }}</td>
+            <td>{{ $data->nama }}</td>
+            <td>{{ $data->level_id }}</td>
+        </tr> --}}
+        {{-- @endforeach --}}
+        {{-- @else
+        <tr>
+            <td colspan="4">Data tidak ditemukan</td>
+        </tr>
+        @endif --}}
+        <tr>
+            <th>Jumlah Pengguna</th>
+        </tr>
+        <tr>
+            <td>{{ $data }}</td>
+        </tr>
+
     </table>
 </body>
 
