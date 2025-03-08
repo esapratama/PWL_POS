246d177f706fb972960467a6bbc4e9742ab08e53
Esa Pratama Putri <ptrprtm16@gmail.com>
2025-03-08 12:34:15 +0700

    praktikum 1 j4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,13 +10,16 @@ class UserController extends Controller
 {
     public function index()
     {
-        // tambah data user dengan Eloquent Model
+        //jobsheet 4
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('1234'),
         ];
-        UserModel::where('username', 'customer-1')->update($data); // update data user
+        UserModel::create($data);
 
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        $user = UserModel::all(); 
         return view('user', ['data' => $user]);
     }
 }
--- database/migrations/2025_02_24_065058_create_m_kategori_table.php
+++ database/migrations/2025_02_24_065058_create_m_kategori_table.php
@@ -7,9 +7,12 @@ use Illuminate\Support\Facades\Schema;
 class CreateMKategoriTable extends Migration {
     public function up(): void {
         Schema::create('m_kategori', function (Blueprint $table) {
-            $table->id();
-            $table->string('kategori_kode');
-            $table->string('kategori_nama');
+            // $table->id();
+            // $table->string('kategori_kode');
+            // $table->string('kategori_nama');
+            // $table->timestamps();
+            $table->id('kategori_id'); // Pastikan ada ID utama
+            $table->string('nama_kategori');
             $table->timestamps();
         });
     }
