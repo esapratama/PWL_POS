1f1b121ea559390ce39cf670c07a5d949229be48
Esa Pratama Putri <ptrprtm16@gmail.com>
2025-04-18 19:42:10 +0700

    J10 Praktikum 1 – Membuat RESTful API Register

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,48 +2,49 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
 
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+    
+    use HasFactory;
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
+    protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'updated_at'];
 
-    protected $fillable = [
-        'level_id',
-        'username',
-        'nama',
-        'password',
-        'created_at',
-        'updated_at'
-    ];
+    protected $hidden = ['password']; // jangan di tampilkan saat select
 
-    protected $hidden = ['password'];
+    protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
 
-    protected $casts = ['password' => 'hashed'];
-
-    public function level():BelongsTo
+    public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
 
-    public function getRoleName(): string
-    {
-        return $this -> level -> level_nama;
+    public function getRoleName(): string {
+        return $this->level->level_nama;
     }
 
-    public function hasRole($role): bool
-    {
-        return $this -> level -> level_kode == $role;
+    public function hasRole($role): bool {
+        return $this->level->level_kode == $role;
     }
 
-    public function getRole()
-    {
+    public function getRole() {
         return $this->level->level_kode;
     }
 }
+?>
